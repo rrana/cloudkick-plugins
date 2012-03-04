aced4a3d41949cefcfb07f1fface20e7b645ae29
Khandkar Mamunur <rana@newscred.com>
2012-03-04 12:23:12 +0600

    First Commit

---
+++ process_monitoring_plugin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Name of the process to be monitored
+PROCESS_NAME="mongo"
+
+NUM_PROCESSES=`ps ax | grep "$PROCESS_NAME" | grep -v grep | wc -l`
+
+if [ "$NUM_PROCESSES" -lt "1" ]; then
+  echo "status err $PROCESS_NAME is not running"
+else
+  echo "status ok $PROCESS_NAME is running"
+fi
