2e1d44e4bae3a0d4fe43e450905c57d4747d673c
Rana <rana@newscred.com>
2012-10-25 10:27:28 +0600

    prod-celery cloudkick custom plugin updated with clustering.py with a timeout of 90 min (5400s)

---
+++ celery/prod-celery/celeryd_misc_tasks.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys, os, time
+
+#SET The maximum log freshness time here
+DEFAULT_AGE=3600
+
+def check_logs(*logs):
+  out = []
+  total = 0
+  status = 'ok'
+  msg = 'everything looks good'
+  n = time.time()
+  for l in logs:
+    if isinstance(l, (list, tuple)):
+      limit, l = l
+    else:
+      limit = DEFAULT_AGE
+    try:
+      s = os.stat(l)
+    except Exception:
+      status = 'err'
+      msg = "file not found '%s'" % l
+    else:
+      diff = n - (s.st_mtime)
+      msg = 'everything looks good, modified %d seconds ago' % diff
+      if diff > limit:
+        status = 'err'
+        msg = '%s not modified in %d seconds' % (l, diff)
+
+  out.insert(0, "status %s %s" % (status, msg))
+  print '\n'.join(out)
+
+if len(sys.argv) < 2:
+  sys.exit('Usage: %s <log file to check>' % sys.argv[0])
+
+if not os.path.exists(sys.argv[1]):
+  sys.exit('status err file %s not found' % (sys.argv[1]))
+else:
+  check_logs(sys.argv[1])
